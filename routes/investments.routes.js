d06c47b7a56b1916015eb29bdd86e7062d02432d
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2025-03-05 07:03:16 +0100

    Create investment transactions search route

--- routes/investments.routes.js
+++ routes/investments.routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const route = express.Router();
 const { validation } = require('../middleware/auth');
 const isAdmin = require('../middleware/admin.middleware');
-const searchInvestments = require('../controllers/investments.controllers');
+const {
+  searchInvestments,
+  fetchInvestmentTransactions
+} = require('../controllers/investments.controllers');
 
 route.post('/search', validation, isAdmin, searchInvestments);
-route.post('/transactions/search', validation, );
+route.post('/transactions/search', validation, fetchInvestmentTransactions);
 route.get('/:investmentId', validation, );
 
 module.exports = route;
